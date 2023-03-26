312bc44b93abce4842a64998607f12d8fa5c4c1c
Elena Litvinova <ivanovaev90@gmail.com>
2023-03-26 21:17:22 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import WolfGallery from './WolfGallery';
+
+import './App.css';
+
+function App() {
+  return (
+    <main id="main">
+      <WolfGallery />
+    </main>
+  )
+}
+
+export default App;
---
+++ src/WolfGallery/index.tsx
@@ -0,0 +1,7 @@
+export default function WolfGallery() {
+    return (
+        <div>
+            My gallery zdv zdvzzsd zsdv zsdfsdf sdasfas zdfd asdfv sdf ad asdf My gallery zdv zdvzzsd zsdv zsdfsdf sdasfas zdfd asdfv sdf ad asdf
+        </div>
+    )
+}
