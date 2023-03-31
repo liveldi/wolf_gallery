af8e8c915ea7a38d13f142e1260cd3d998bc50c4
Elena Litvinova <ivanovaev90@gmail.com>
2023-03-31 19:54:34 +0100

    Added first styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import WolfGallery from './WolfGallery';
+// Gallery based on https://www.aspinallfoundation.org/port-lympne/short-breaks/wolf-lodge/
 
-import './App.css';
+import { WolfGallery } from './WolfGallery';
+
+import './styles/reset.css';
+import './styles/index.css';
+
 
 function App() {
   return (
     <main id="main">
-      <WolfGallery />
+      <WolfGallery
+        photos={[
+          { id: 1, src: '/photos/1.jpg', description: 'Norway 1' },
+          { id: 2, src: '/photos/2.jpg', description: 'Norway 2' },
+          { id: 3, src: '/photos/3.jpg', description: 'Norway 3' },
+          { id: 4, src: '/photos/4.jpg', description: 'Norway 4' },
+          { id: 5, src: '/photos/5.jpg', description: 'Norway 5' },
+          { id: 6, src: '/photos/6.jpg', description: 'Norway 6' },
+          { id: 7, src: '/photos/7.jpg', description: 'Norway 7' },
+        ]}
+      />
     </main>
   )
 }
--- src/WolfGallery/index.tsx
+++ src/WolfGallery/index.tsx
@@ -1,7 +1,48 @@
-export default function WolfGallery() {
+import {useState} from 'react';
+import style from './index.module.scss';
+
+interface Photo {
+    id: number;
+    src: string;
+    description: string;
+}
+interface WolfGalleryProps {
+    photos: Photo[],
+}
+
+console.log(style)
+
+export const WolfGallery: React.FC<WolfGalleryProps> = ({
+    photos,
+}) => {
+    if (!photos.length) {
+        return null;
+    }
+
+    const [ indexActivePhoto, setIndexActivePhoto ] = useState(0);
+
     return (
-        <div>
-            My gallery zdv zdvzzsd zsdv zsdfsdf sdasfas zdfd asdfv sdf ad asdf My gallery zdv zdvzzsd zsdv zsdfsdf sdasfas zdfd asdfv sdf ad asdf
+        <div className={style.wolfGallery}>
+            <div className={style.wolfGalleryContainer}>
+
+            </div>
+            <div className={style.wolfGalleryWrapper}>
+                <ul className={style.wolfGalleryTrack}>
+                    {photos.map((photo) => (
+                        <li
+                            key={photo.id}
+                            className={style.wolfGalleryPreview}
+                        >
+                            <img
+                                src={photo.src}
+                                alt={photo.description}
+                                loading="lazy"
+                                className={style.wolfGalleryPreview}
+                            />
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
